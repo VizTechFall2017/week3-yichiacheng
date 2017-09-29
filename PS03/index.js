b357614dfc2c3bc65e87ff4f0bfeed047da49aa6
yichiacheng <31904793+yichiacheng@users.noreply.github.com>
2017-09-29 13:07:53 -0400

    PS03

--- PS03/index.js
+++ PS03/index.js
@@ -3,67 +3,52 @@ var svg = d3.select('svg');
 /* Your code goes here */
 
 
-d3.csv("boston1bed".csv, function(data){
-    console.debug(JSON.stringify(data));
-    dataset = data;
+d3.csv("boston1bed".csv, function(dataIn){
+    console.log(dataIn);
 
-    var w = 600,h = 500,padding = 30, barMargin = 2;
-
-    var Ymax = d3.max(dataset, function(d){return d.value}),
-        Ymin = d3.min(dataset, function(d){return d.value});
-
-    var xScale = d3.scale.linear()
-        .domain([0, dataset.length])
-        .range([padding , w - padding]);
-
-    var yScale = d3.scale.linear()
-        .domain([Ymin, Ymax])
-        .range([padding, h - padding]);
-
-    var colorScale = d3.scale.linear()
-        .domain([Ymin, Ymax])
-        .range([0, 700]);
-
-    var barWidth = (w - padding*2) / dataset.length - barMargin;
-    var svg = d3.select('Boston1bedrent').append('svg').attr({'width': w,'height': h})
-
-    svg.selectAll('rect')
-        .data(dataset)
-        .enter()
-        .append('rect')
-        .attr({
-            'x': function(d, i){return xScale(i)},
-            'y': function(d){return h - yScale(d.value)},
-            'width': barWidth,
-            'height':function(d){return yScale(d.value)},
-            'r': function(d){return Math.sqrt(h - d[1])},
-            'fill': function(d){
-                var color = 0.2 + colorScale(d.value) * 0.001;
-                return  d3.hsl(200 ,color, color);
-            },
-            'title': function(d){
-                return 'Name : ' + d.name;
-            }
-
-        });
-
-    svg.selectAll('text').data(dataset).enter()
-        .append('text')
-        .text(function(d){ return d.value})
-        .attr({
-            'x': function(d, i){return xScale(i) + barWidth/2},
-            'y': function(d){return h - yScale(d.value) + 15},
-        });
-    svg.append('g').selectAll('text').data(dataset).enter()
-        .append('text')
-        .text(function(d){ return d.name})
-        .attr({
-            'x': function(d, i){return xScale(i) + barWidth/2},
-            'y': function(d){return h - yScale(d.value) - 10},
-
-        });
-});
+    var margin = {top: 20, right: 20, bottom: 70, left: 40},
+        width = 700 - margin.left - margin.right,
+        height = 500 - margin.top - margin.bottom;
 
 
+    var x = d3.scaleBand().rangeRound([0, width]).padding(0.1),
+        y = d3.scaleLinear().rangeRound([height, 0]);
+
+    var g = svg.append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    d3.csv("boston1bed.csv", function(d) {
+        d.frequency = +d.frequency;
+        return d;
+    }, function(error, data) {
+        if (error) throw error;
+
+        x.domain(data.map(function(d) { return d.letter; }));
+        y.domain([0, d3.max(data, function(d) { return d.frequency; })]);
+
+        g.append("g")
+            .attr("class", "axis axis--x")
+            .attr("transform", "translate(0," + height + ")")
+            .call(d3.axisBottom(x));
+
+        g.append("g")
+            .attr("class", "axis axis--y")
+            .call(d3.axisLeft(y).ticks(10, "%"))
+            .append("text")
+            .attr("transform", "rotate(-90)")
+            .attr("y", 6)
+            .attr("dy", "0.71em")
+            .attr("text-anchor", "end")
+            .text("Frequency");
+
+        g.selectAll(".bar")
+            .data(data)
+            .enter().append("rect")
+            .attr("class", "bar")
+            .attr("x", function(d) { return x(d.letter); })
+            .attr("y", function(d) { return y(d.frequency); })
+            .attr("width", x.bandwidth())
+            .attr("height", function(d) { return height - y(d.frequency)
+            });
 
 
